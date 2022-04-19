b3d6acd0e68cebee79567a7b48f199448c5ff47c
Landon Cayia <lcayia@uvm.edu>
2022-04-18 20:30:43 -0400

    Add app.py, Flask code that implements functions in db file

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import db
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/api/pokemon', methods=['GET'])
+def api_get_all_pokemon():
+    return jsonify(db.get_all_pokemon())
+
+@app.route('/api/pokemon/<pokedex_no>', methods=['GET'])
+def api_get_pokemon_by_dex(pokedex_no):
+    return jsonify(db.get_pokemon_by_dex(pokedex_no))
+
+@app.route('api/pokemon/add', methods=['POST'])
+def api_add_pokemon():
+    pokemon = request.get_json()
+    return jsonify(db.insert_pokemon(pokemon))
+
+@app.route('api/pokemon/update', methods=['PUT'])
+def api_update_pokemon():
+    pokemon = request.get_json()
+    return jsonify(db.update_pokemon(pokemon))
+
+@app.route('api/pokemon/delete/<pokedex_no>', methods=['DELETE'])
+def api_delete_pokemon(pokedex_no):
+    return jsonify(db.delete_pokemon(pokedex_no))
+
+if __name__ == "__main__":
+    #app.debug = True
+    #app.run(debug=True)
+    app.run() # run Flask app
