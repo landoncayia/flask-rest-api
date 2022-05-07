0bd5c09666f71044526e9e3d3ba253c76ba253db
Landon Cayia <lcayia@uvm.edu>
2022-05-07 11:48:23 -0400

    Add root page and script required to run on Silk

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask, request, jsonify
-# from flask_cors import CORS
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 import db
 
-db.create_db_table()
-db.csv_to_db()
+# Create the Pokemon table in the database file
+#db.create_db_table()
+# Add the data from the Pokemon csv to the database
+#db.csv_to_db()
 
 app = Flask(__name__)
 # Allow access to endpoints from any IP address
-# CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/')
+def home():
+    return """Welcome to the Pokemon REST API. Here are the utilities available to you:<br>
+    \t* /api/pokemon: Return all Pokemon using a GET request<br>
+    \t* /api/pokemon/<pokedex_no>: Return a single Pokemon using a GET request (via Pokedex number)<br>
+    \t* /api/pokemon/add: Add a Pokemon using a POST request (new data should be in JSON form)<br>
+    \t* /api/pokemon/update: Update a Pokemon using a PUT request (updates made by linking Pokedex numbers)<br>
+    \t* /api/pokemon/delete/<pokedex_no>: Delete a Pokemon using a DELETE request (via Pokedex number)<br>
+    """
 
 @app.route('/api/pokemon', methods=['GET'])
 def api_get_all_pokemon():
@@ -37,4 +49,4 @@ def api_delete_pokemon(pokedex_no):
 if __name__ == "__main__":
     #app.debug = True
     #app.run(debug=True)
-    app.run(host='0.0.0.0') # run Flask app
+    app.run() # run Flask app
